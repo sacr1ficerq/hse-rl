d3b7bea05267c4d0f8c5950cfca717318d749ca7
Dubinin Daniil <sacr1ficerq@gmail.com>
2026-01-09 14:15:49 +0300

    HW2 SARSA and EV-SARSA implementations

--- HW2/src/qlearning.py
+++ HW2/src/qlearning.py
@@ -59,3 +59,48 @@ class QLearningAgent(Agent):
 
         self.q_values[state][action] += self.lr * td_error
         self.mse.append(td_error**2)
+
+
+class SarsaAgent(Agent):
+    def __init__(
+        self,
+        n_actions: int,
+        learning_rate: float,
+        epsilon: float,
+        decay_factor: float,
+        discount_factor: float = 0.95,
+    ):
+        super().__init__(n_actions, learning_rate, epsilon, discount_factor)
+        self.decay_factor = decay_factor
+        self.on_episode_start()
+
+    def on_episode_start(self):
+        self.e_traces = defaultdict(lambda: np.zeros(self.n_actions))
+
+    def get_value(self, state: StateType) -> float:
+        action = self.get_action(state)
+        return self.q_values[state][action]
+
+    def update(
+        self,
+        state: StateType,
+        action: int,
+        reward: float,
+        terminated: bool,
+        next_state: StateType,
+    ):
+        next_q_value = (1 - terminated) * self.get_value(next_state)
+        y = reward + self.discount_factor * next_q_value
+        td_error = y - self.q_values[state]
+        self.e_traces[state][action] += 1
+        for s in self.e_traces:
+            self.q_values[s] += self.lr * self.e_traces[state] * td_error
+            self.e_traces[s] *= self.discount_factor * self.decay_factor
+        self.mse.append(td_error**2)
+
+
+class EVSarsaAgent(SarsaAgent):
+    def get_value(self, state: StateType) -> float:
+        state_q_values = self.q_values[state]
+        value = self.epsilon * np.mean(state_q_values) + (1 - self.epsilon) * np.max(state_q_values)
+        return value  # type: ignore
