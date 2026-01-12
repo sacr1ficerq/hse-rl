b45c9265bfc21435535014119f25c071918fb334
Dubinin Daniil <sacr1ficerq@gmail.com>
2026-01-12 09:59:41 +0300

    HW3 Dueling Network implementation

---
+++ DQN/src/DQN.py
@@ -0,0 +1,179 @@
+import torch
+import torch.nn as nn
+
+import numpy as np
+
+
+def conv2d_size_out(size: int, kernel_size: int, stride: int) -> int:
+    """
+    common use case:
+    cur_layer_img_w = conv2d_size_out(cur_layer_img_w, kernel_size, stride)
+    cur_layer_img_h = conv2d_size_out(cur_layer_img_h, kernel_size, stride)
+    to understand the shape for dense layer's input
+    """
+    return (size - (kernel_size - 1) - 1) // stride + 1
+
+DEBUG = False
+
+class DuelingNetwork(nn.Module):
+    """
+    Implement the Dueling DQN logic.
+    """
+
+    def __init__(self, n_actions, width, height, hidden_size) -> None:
+        super().__init__()
+        self.n_actions = n_actions
+        self.width = width
+        self.height = height
+        self.hidden_size = hidden_size
+
+        # <YOUR_CODE>
+        self.convolution = nn.Sequential(
+            nn.Conv2d(in_channels= 4, out_channels=16, kernel_size=3, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2),
+            nn.ReLU(),
+        )
+
+        convolve = lambda size: conv2d_size_out(size, kernel_size=3, stride=2)  # type: ignore
+
+        width = convolve(convolve(convolve(width)))
+        height = convolve(convolve(convolve(height)))
+        out_features = width * height * 64
+
+        self.value_layer = nn.Sequential(
+            nn.Linear(out_features, hidden_size),  # hidden_size?
+            nn.ReLU(),
+            nn.Linear(hidden_size, 1)  # hidden_size?
+        )
+
+        self.advantage_layer = nn.Sequential(
+            nn.Linear(out_features, hidden_size),  # hidden_size?
+            nn.ReLU(),
+            nn.Linear(hidden_size, n_actions)  # hidden_size?
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        assert x.ndim == 4, x.shape  # (batch_size, n_images, width, height)
+        # When calculating the mean advantage, please, remember, x is a batched input!
+
+        # <YOUR_CODE>
+        features = self.convolution(x)  # (batch_size, 64, out_features)
+        features = features.flatten(start_dim=1)  # (batch_size, out_features)
+
+        value = self.value_layer(features).squeeze(dim=1)  # (batch_size, 1)
+        advantage = self.advantage_layer(features)
+
+        advantage -= advantage.mean(dim=1, keepdim=True)  # ?
+        if DEBUG: print(f"advantage shape: {advantage.shape}")
+
+        return advantage + value  # repeat value?
+
+
+class GradScalerFunctional(torch.autograd.Function):
+    """
+    A torch.autograd.Function works as Identity on forward pass
+    and scales the gradient by scale_factor on backward pass.
+    """
+    @staticmethod
+    def forward(ctx, input, scale_factor):
+        ctx.scale_factor = scale_factor
+        return input
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        scale_factor = ctx.scale_factor
+        grad_input = grad_output * scale_factor
+        return grad_input, None
+
+
+class GradScaler(nn.Module):
+    """
+    An nn.Module incapsulating GradScalerFunctional
+    """
+    def __init__(self, scale_factor: float):
+        super().__init__()
+        self.scale_factor = scale_factor
+
+    def forward(self, x):
+        return GradScalerFunctional.apply(x, self.scale_factor)
+
+
+class DQNAgent(nn.Module):
+    def __init__(self, state_shape, n_actions, epsilon=0.0, hidden_size=32, device='cuda'):
+        super().__init__()
+        self.device = device
+        self.epsilon = epsilon
+        self.n_actions = n_actions
+        self.state_shape = state_shape
+
+        n_images, width, height = state_shape
+        assert n_images == 4  # Network relies on that
+
+        # Define your network body here. Please make sure agent is fully contained here
+        # nn.Flatten() can be useful
+
+        self.network = DuelingNetwork(n_actions, width, height, hidden_size).to(device)
+
+    def forward(self, state_t):
+        """
+        takes agent's observation (tensor), returns qvalues (tensor)
+        :param state_t: a batch of 4-frame buffers, shape = [batch_size, 4, h, w]
+        """
+        # Use your network to compute qvalues for given state
+        q_values = self.network(state_t.to(self.device))
+        return q_values
+
+    @torch.inference_mode()
+    def get_qvalues(self, states: np.ndarray) -> np.ndarray:
+        """
+        like forward, but works on numpy arrays, not tensors
+        """
+        tensor = torch.from_numpy(states).to(self.device)
+        if DEBUG: print(f"Tensor shape: {tensor.shape}")
+        with torch.no_grad():
+            q_values = self.network(tensor)
+        if DEBUG: print(f"q_values shape: {q_values.shape}")
+        return q_values.detach().cpu().numpy()
+
+    def sample_actions_by_qvalues(self, qvalues: np.ndarray, greedy: bool = False) -> np.ndarray:
+        """pick actions given qvalues based on epsilon-greedy exploration strategy."""
+        batch_size, n_actions = qvalues.shape
+        eps = self.epsilon
+
+        greedy_actions = np.argmax(qvalues, axis=1)
+
+        if greedy:
+            return greedy_actions
+
+        rand = np.random.choice(a=n_actions, size=batch_size, replace=True)
+        return np.where(np.random.random(batch_size) < eps, rand, greedy_actions)
+
+    def sample_actions(self, states: np.ndarray, greedy: bool = False) -> np.ndarray:
+        if isinstance(states, list):
+            states = np.array(states)
+        qvalues = self.get_qvalues(states)
+
+        if DEBUG: print(f"Q-values shape: {qvalues.shape}")
+
+        return self.sample_actions_by_qvalues(qvalues, greedy)
+
+
+if __name__ == '__main__':
+    device = torch.device('cuda:0')
+    module = DuelingNetwork(n_actions=4, width=16, height=16, hidden_size=32).to(device)
+    if DEBUG: print(module)
+
+    x = torch.randn(size=(1, 4, 16, 16), device=device)  # (batch_size, C, width, height))
+    if DEBUG: print(module(x))
+
+    n_actions = 5
+    state_shape = (4, 16, 16)
+    epsilon = 0.1
+    hidden_size = 32
+    agent = DQNAgent(state_shape, n_actions, epsilon, hidden_size)
+
+    npx = x.detach().cpu().numpy()
+    agent.get_qvalues(npx)
